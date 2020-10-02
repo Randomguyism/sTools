fb0f83f3717f3a433ad481e26ed75bd868c2cfc9
Randomguyism <72268727+Randomguyism@users.noreply.github.com>
2020-10-02 16:57:34 +0100

    Util

---
+++ sTools_Utilities.py
@@ -0,0 +1,23 @@
+from maya import cmds as cmds
+
+def tabLayout(name, parent, width, scrollBarThickness):
+		tab = cmds.scrollLayout(parent = parent, verticalScrollBarThickness = scrollBarThickness, verticalScrollBarAlwaysVisible = True)
+		cmds.columnLayout(adjustableColumn = True,  width = width)
+		cmds.tabLayout(parent, edit = True, tabLabel = (tab, name))
+
+def toolTitle(label, ann, spacing):
+		cmds.separator(style = 'in', horizontal = True, height = spacing[0])
+		cmds.text(label = label, ann = ann, font = 'boldLabelFont', height = spacing[1])
+		cmds.separator(style = 'out', horizontal = True, height = spacing[0])
+
+def test(*args):
+	# Test function
+	print('This funciton was called correctly.')
+
+def printButton(data, *args):
+	# Test function
+	print(data)
+
+def printData(data):
+	# Test function
+	print(data)
